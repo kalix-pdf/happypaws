a0e08d0fe8f774d60537c0ee4abc4893532f781c
Jake <ejakearingo@gmail.com>
2025-05-20 23:06:21 +0800

    UI ni hp na!

--- config.php
+++ config.php
@@ -26,7 +26,7 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'smesoftc_ocart3x0x3x9');
+define('DB_DATABASE', 'smesoftc_hp');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
---
+++ storage/cache/template/cf/cf454edf833bff6e27072e6652704d04fc64844df473f0be5fd0b207469ff642.php
@@ -0,0 +1,396 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__d07b68b1cc3016dcc23773d841dfe38fa5d58d3d6841618af3d75ce6bc0ec714 */
+class __TwigTemplate_71223715cb2496f55a859da5f0c4583ee4a95a10023489a230c45364b5c4560b extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<!--[if IE]><![endif]-->
+<!--[if IE 8 ]><html dir=\"";
+        // line 3
+        echo ($context["direction"] ?? null);
+        echo "\" lang=\"";
+        echo ($context["lang"] ?? null);
+        echo "\" class=\"ie8\"><![endif]-->
+<!--[if IE 9 ]><html dir=\"";
+        // line 4
+        echo ($context["direction"] ?? null);
+        echo "\" lang=\"";
+        echo ($context["lang"] ?? null);
+        echo "\" class=\"ie9\"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html dir=\"";
+        // line 6
+        echo ($context["direction"] ?? null);
+        echo "\" lang=\"";
+        echo ($context["lang"] ?? null);
+        echo "\">
+<!--<![endif]-->
+<head>
+
+      ";
+        // line 10
+        if ((array_key_exists("module_marketplace_status", $context) && ($context["module_marketplace_status"] ?? null))) {
+            // line 11
+            echo "      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject'] = r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      </script>
+      ";
+        }
+        // line 18
+        echo "      
+<meta charset=\"UTF-8\" />
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+<title>";
+        // line 22
+        echo ($context["title"] ?? null);
+        echo "</title>
+<base href=\"";
+        // line 23
+        echo ($context["base"] ?? null);
+        echo "\" />
+";
+        // line 24
+        if (($context["description"] ?? null)) {
+            // line 25
+            echo "<meta name=\"description\" content=\"";
+            echo ($context["description"] ?? null);
+            echo "\" />
+";
+        }
+        // line 27
+        if (($context["keywords"] ?? null)) {
+            // line 28
+            echo "<meta name=\"keywords\" content=\"";
+            echo ($context["keywords"] ?? null);
+            echo "\" />
+";
+        }
+        // line 30
+        echo "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT\" crossorigin=\"anonymous\">
+<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO\" crossorigin=\"anonymous\"></script>
+
+";
+        // line 33
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["styles"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["style"]) {
+            // line 34
+            echo "<link href=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["style"], "href", [], "any", false, false, false, 34);
+            echo "\" type=\"text/css\" rel=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["style"], "rel", [], "any", false, false, false, 34);
+            echo "\" media=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["style"], "media", [], "any", false, false, false, 34);
+            echo "\" />
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['style'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 36
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["scripts"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["script"]) {
+            // line 37
+            echo "<script src=\"";
+            echo $context["script"];
+            echo "\" type=\"text/javascript\"></script>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['script'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 39
+        echo "<script src=\"catalog/view/javascript/common.js\" type=\"text/javascript\"></script>
+";
+        // line 40
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["links"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["link"]) {
+            // line 41
+            echo "<link href=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["link"], "href", [], "any", false, false, false, 41);
+            echo "\" rel=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["link"], "rel", [], "any", false, false, false, 41);
+            echo "\" />
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['link'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 43
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["analytics"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["analytic"]) {
+            // line 44
+            echo $context["analytic"];
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['analytic'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 46
+        echo "
+        <link href=\"https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen\" />
+      
+</head>
+<body>
+
+            <!-- Mobikul code -->
+            ";
+        // line 53
+        if ((array_key_exists("playstore_link", $context) || array_key_exists("appstore_link", $context))) {
+            // line 54
+            echo "            <div class=\"app-link text-center\">
+            ";
+            // line 55
+            echo ($context["front_text"] ?? null);
+            echo "
+            ";
+            // line 56
+            if (($context["playstore_link"] ?? null)) {
+                echo "<a href=\"";
+                echo ($context["playstore_link"] ?? null);
+                echo "\"><img src=\"";
+                echo ($context["playstore_icon"] ?? null);
+                echo "\"></a>";
+            }
+            // line 57
+            echo "            ";
+            if (($context["appstore_link"] ?? null)) {
+                echo "<a href=\"";
+                echo ($context["appstore_link"] ?? null);
+                echo "\"><img src=\"";
+                echo ($context["appstore_icon"] ?? null);
+                echo "\"></a>";
+            }
+            // line 58
+            echo "            </div>
+            ";
+        }
+        // line 60
+        echo "            
+
+          ";
+        // line 62
+        if ((array_key_exists("notification", $context) && ($context["notification"] ?? null))) {
+            echo " ";
+            echo ($context["notification"] ?? null);
+            echo " ";
+        }
+        // line 63
+        echo "      
+<nav id=\"top\">
+  <div class=\"container\">";
+        // line 65
+        echo ($context["currency"] ?? null);
+        echo "
+    ";
+        // line 66
+        echo ($context["language"] ?? null);
+        echo "
+    <div id=\"top-links\" class=\"nav pull-right\">
+      <ul class=\"list-inline\">
+        <li><a href=\"";
+        // line 69
+        echo ($context["contact"] ?? null);
+        echo "\"><i class=\"fa fa-phone\"></i></a> <span class=\"hidden-xs hidden-sm hidden-md\">";
+        echo ($context["telephone"] ?? null);
+        echo "</span></li>
+
+        ";
+        // line 71
+        echo ($context["sellmenu"] ?? null);
+        echo "
+      
+        <li class=\"dropdown\"><a href=\"";
+        // line 73
+        echo ($context["account"] ?? null);
+        echo "\" title=\"";
+        echo ($context["text_account"] ?? null);
+        echo "\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"fa fa-user\"></i> <span class=\"hidden-xs hidden-sm hidden-md\">";
+        echo ($context["text_account"] ?? null);
+        echo "</span> <span class=\"caret\"></span></a>
+          <ul class=\"dropdown-menu dropdown-menu-right\">
+            ";
+        // line 75
+        if (($context["logged"] ?? null)) {
+            // line 76
+            echo "            <li><a href=\"";
+            echo ($context["account"] ?? null);
+            echo "\">";
+            echo ($context["text_account"] ?? null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 77
+            echo ($context["order"] ?? null);
+            echo "\">";
+            echo ($context["text_order"] ?? null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 78
+            echo ($context["transaction"] ?? null);
+            echo "\">";
+            echo ($context["text_transaction"] ?? null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 79
+            echo ($context["download"] ?? null);
+            echo "\">";
+            echo ($context["text_download"] ?? null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 80
+            echo ($context["logout"] ?? null);
+            echo "\">";
+            echo ($context["text_logout"] ?? null);
+            echo "</a></li>
+            ";
+        } else {
+            // line 82
+            echo "            <li><a href=\"";
+            echo ($context["register"] ?? null);
+            echo "\">";
+            echo ($context["text_register"] ?? null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 83
+            echo ($context["login"] ?? null);
+            echo "\">";
+            echo ($context["text_login"] ?? null);
+            echo "</a></li>
+            ";
+        }
+        // line 85
+        echo "          </ul>
+        </li>
+        <li><a href=\"";
+        // line 87
+        echo ($context["wishlist"] ?? null);
+        echo "\" id=\"wishlist-total\" title=\"";
+        echo ($context["text_wishlist"] ?? null);
+        echo "\"><i class=\"fa fa-heart\"></i> <span class=\"hidden-xs hidden-sm hidden-md\">";
+        echo ($context["text_wishlist"] ?? null);
+        echo "</span></a></li>
+        <li><a href=\"";
+        // line 88
+        echo ($context["shopping_cart"] ?? null);
+        echo "\" title=\"";
+        echo ($context["text_shopping_cart"] ?? null);
+        echo "\"><i class=\"fa fa-shopping-cart\"></i> <span class=\"hidden-xs hidden-sm hidden-md\">";
+        echo ($context["text_shopping_cart"] ?? null);
+        echo "</span></a></li>
+        <li><a href=\"";
+        // line 89
+        echo ($context["checkout"] ?? null);
+        echo "\" title=\"";
+        echo ($context["text_checkout"] ?? null);
+        echo "\"><i class=\"fa fa-share\"></i> <span class=\"hidden-xs hidden-sm hidden-md\">";
+        echo ($context["text_checkout"] ?? null);
+        echo "</span></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<header>
+  <div class=\"container\">
+    <div class=\"row\">
+      <div class=\"col-sm-4\">
+        <div id=\"logo\">";
+        // line 98
+        if (($context["logo"] ?? null)) {
+            echo "<a href=\"";
+            echo ($context["home"] ?? null);
+            echo "\"><img src=\"";
+            echo ($context["logo"] ?? null);
+            echo "\" title=\"";
+            echo ($context["name"] ?? null);
+            echo "\" alt=\"";
+            echo ($context["name"] ?? null);
+            echo "\" class=\"img-responsive\" /></a>";
+        } else {
+            // line 99
+            echo "          <h1><a href=\"";
+            echo ($context["home"] ?? null);
+            echo "\">";
+            echo ($context["name"] ?? null);
+            echo "</a></h1>
+          ";
+        }
+        // line 100
+        echo "</div>
+      </div>
+      <div class=\"col-sm-5\">";
+        // line 102
+        echo ($context["search"] ?? null);
+        echo "</div>
+      <div class=\"col-sm-3\">";
+        // line 103
+        echo ($context["cart"] ?? null);
+        echo "</div>
+    </div>
+  </div>
+</header>
+";
+        // line 107
+        echo ($context["menu"] ?? null);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__d07b68b1cc3016dcc23773d841dfe38fa5d58d3d6841618af3d75ce6bc0ec714";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  372 => 107,  365 => 103,  361 => 102,  357 => 100,  349 => 99,  337 => 98,  321 => 89,  313 => 88,  305 => 87,  301 => 85,  294 => 83,  287 => 82,  280 => 80,  274 => 79,  268 => 78,  262 => 77,  255 => 76,  253 => 75,  244 => 73,  239 => 71,  232 => 69,  226 => 66,  222 => 65,  218 => 63,  212 => 62,  208 => 60,  204 => 58,  195 => 57,  187 => 56,  183 => 55,  180 => 54,  178 => 53,  169 => 46,  161 => 44,  157 => 43,  146 => 41,  142 => 40,  139 => 39,  130 => 37,  126 => 36,  113 => 34,  109 => 33,  104 => 30,  98 => 28,  96 => 27,  90 => 25,  88 => 24,  84 => 23,  80 => 22,  74 => 18,  65 => 11,  63 => 10,  54 => 6,  47 => 4,  41 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__d07b68b1cc3016dcc23773d841dfe38fa5d58d3d6841618af3d75ce6bc0ec714", "");
+    }
+}
